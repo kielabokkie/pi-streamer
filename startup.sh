5674d79ab455b64fcdc5c58d86965e30f61dea97
kielabokkie <kielabokkie@gmail.com>
2017-09-05 13:24:04 +0200

    Added startup script

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script gets the latest version of the docker image and starts streaming
+
+cd ~/pi-streamer
+
+echo "Pulling changes"
+git pull
+
+echo "Building docker image"
+docker build -t kielabokkie/pi-streamer .
+
+echo "Remove old docker container"
+docker rm -f cam
+
+echo "Starting Youtube stream"
+docker run --privileged --name cam -d kielabokkie/pi-streamer $YOUTUBE_KEY
