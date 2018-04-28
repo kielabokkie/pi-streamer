ec46f0bd3b3b9e8c5d60c61211bd4eb03dceec8d
kielabokkie <kielabokkie@gmail.com>
2018-04-28 16:18:39 +1200

    Add date and time overlay and update ffmpeg options

--- entry.sh
+++ entry.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo Live-stream secret: $1
+echo "Starting YouTube live-stream"
 
-raspivid -o - -t 0 -w 640 -h 480 -fps 24 -b 2000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$1
+FONT="/usr/local/share/fonts/OpenSans-Regular.ttf"
+FONTSIZE="15"
+TEXTCOLOR="white@0.8"
+X="10"
+Y="440"
+
+raspivid -o - -t 0 -w 640 -h 480 -fps 24 -b 2000000 | ffmpeg -re -f s16le -i /dev/zero -f h264 -i pipe:0 -c:v copy -an -preset superfast -vf "drawtext=fontfile=${FONT}:fontcolor=${TEXTCOLOR}:fontsize=${FONTSIZE}:x=${X}:y=${Y}:text='%{localtime\: %d\/%m\/%y %T}'" -f flv -r 24 rtmp://a.rtmp.youtube.com/live2/$1
