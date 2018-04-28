5dc94f5ab4d571b0bf3006e7c4d290568b4f06ab
kielabokkie <kielabokkie@gmail.com>
2018-04-28 17:56:02 +1200

    Add font to docker image

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,7 @@
 FROM alexellis2/streaming:17-5-2017
+
+WORKDIR /usr/local/share/fonts
+ADD fonts/OpenSans-Regular.ttf OpenSans-Regular.ttf
+WORKDIR /
+
 COPY entry.sh entry.sh
--- entry.sh
+++ entry.sh
@@ -8,4 +8,4 @@ TEXTCOLOR="white@0.8"
 X="10"
 Y="440"
 
-raspivid -o - -t 0 -w 640 -h 480 -fps 24 -b 2000000 | ffmpeg -re -f s16le -i /dev/zero -f h264 -i pipe:0 -c:v copy -an -preset superfast -vf "drawtext=fontfile=${FONT}:fontcolor=${TEXTCOLOR}:fontsize=${FONTSIZE}:x=${X}:y=${Y}:text='%{localtime\: %d\/%m\/%y %T}'" -f flv -r 24 rtmp://a.rtmp.youtube.com/live2/$1
+raspivid -o - -t 0 -w 640 -h 480 -fps 24 -b 2000000 | ffmpeg -re -f s16le -i /dev/zero -f h264 -i pipe:0 -c:v copy -an -vf "drawtext=fontfile=${FONT}:fontcolor=${TEXTCOLOR}:fontsize=${FONTSIZE}:x=${X}:y=${Y}:text='%{localtime\: %d\/%m\/%y %T}'" -f flv -r 24 rtmp://a.rtmp.youtube.com/live2/$1
