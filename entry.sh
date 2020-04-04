36f7980af511ec4094f6e7d9c758daaa203f898a
kielabokkie <kielabokkie@gmail.com>
2020-04-04 21:15:56 +1300

    Fix AWB setting

--- entry.sh
+++ entry.sh
@@ -2,4 +2,4 @@
 
 echo "Starting YouTube live-stream"
 
-raspivid -o - -t 0 -w 1280 -h 720 -fps 24 -b 2000000 --awb -ae 20,0xff,0x808000,left -a 12 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f fifo -fifo_format flv -map 1:v -map 0:a -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 -recover_any_error 1 -r 24 rtmp://a.rtmp.youtube.com/live2/$1
+raspivid -o - -t 0 -w 1280 -h 720 -fps 24 -b 2000000 --awb auto -ae 20,0xff,0x808000,left -a 12 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f fifo -fifo_format flv -map 1:v -map 0:a -drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 1 -recover_any_error 1 -r 24 rtmp://a.rtmp.youtube.com/live2/$1
