090232d4e62bf3f44cd8ed65c0a37c8773342e56
kielabokkie <kielabokkie@gmail.com>
2020-05-18 22:46:22 +1200

    Try out Twitch

--- startup.sh
+++ startup.sh
@@ -4,14 +4,17 @@
 
 cd /home/pi/pi-streamer
 
-echo "Pulling changes"
-git pull
+# echo "Pulling changes"
+# git pull
 
-echo "Building docker image"
-docker build -t kielabokkie/pi-streamer .
+# echo "Building docker image"
+# docker build -t kielabokkie/pi-streamer .
 
 echo "Remove old docker container"
 docker rm -f cam
 
-echo "Starting Youtube stream"
-docker run --privileged --restart always --name cam -d kielabokkie/pi-streamer $YOUTUBE_KEY
+# echo "Starting Youtube stream"
+# docker run --privileged --restart always --name cam -d kielabokkie/pi-streamer $YOUTUBE_KEY
+
+echo "Starting Twitch stream"
+docker run --privileged --restart always --name cam -d kielabokkie/pi-streamer $TWITCH_KEY
